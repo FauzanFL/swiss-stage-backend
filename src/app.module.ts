8817d424b65aae72137fb6ae56d0f99684b02052
fauzan.lisan <fauzan.lisan@solusi247.com>
2026-02-19 09:53:38 +0700

    add modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TournamentModule } from './tournament/tournament.module';
+import { ParticipantModule } from './participant/participant.module';
+import { RoundModule } from './round/round.module';
+import { MatchModule } from './match/match.module';
+import { PairingModule } from './pairing/pairing.module';
+import { StandingsModule } from './standings/standings.module';
 
 @Module({
-  imports: [],
+  imports: [TournamentModule, ParticipantModule, RoundModule, MatchModule, PairingModule, StandingsModule],
   controllers: [AppController],
   providers: [AppService],
 })
